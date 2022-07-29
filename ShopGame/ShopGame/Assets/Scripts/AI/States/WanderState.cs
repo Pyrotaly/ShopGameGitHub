048b08e31c37e7e967bbdf10fe7aec03b36b32f7
Pyrotaly <102985554+Pyrotaly@users.noreply.github.com>
2022-07-28 23:38:52 -0400

    AI Tweeek

--- ShopGame/ShopGame/Assets/Scripts/AI/States/WanderState.cs
+++ ShopGame/ShopGame/Assets/Scripts/AI/States/WanderState.cs
@@ -4,5 +4,7 @@ using UnityEngine;
 
 public class WanderState : NPCState
 {
-
+    public WanderState(NPC entity, NPCStatemachine stateMachine, string animBoolName) : base(entity, stateMachine, animBoolName)
+    {
+    }
 }
