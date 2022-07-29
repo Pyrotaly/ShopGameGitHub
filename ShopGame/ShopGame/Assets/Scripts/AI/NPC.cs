174ff58b96ed150da06635a80c4cf43b39cea857
Pyrotaly <102985554+Pyrotaly@users.noreply.github.com>
2022-07-29 14:26:33 -0400

    Made random position generator within area script

--- ShopGame/ShopGame/Assets/Scripts/AI/NPC.cs
+++ ShopGame/ShopGame/Assets/Scripts/AI/NPC.cs
@@ -6,17 +6,26 @@ using Pathfinding;
 public class NPC : MonoBehaviour
 {
     public Animator Anim { get; private set; }
+
     [HideInInspector] public AIPath aiPath;
     public Transform testTransform;
+
+    public RandomPositionDestination rpd;
 
     protected virtual void Awake()
     {
         Anim = GetComponent<Animator>();
         aiPath = GetComponent<AIPath>();
+        rpd = GetComponent<RandomPositionDestination>();
     }
 
-    protected void Update()
+    protected void Start()
     {
-        aiPath.destination = testTransform.position;
+        aiPath.destination = rpd.GeneratePosition();
     }
+
+    //protected void Update()
+    //{
+    //    aiPath.destination = testTransform.position;
+    //}
 }
