c95c9fb56a8cae276955a212c44a56e498e0aa33
Pyrotaly <102985554+Pyrotaly@users.noreply.github.com>
2022-08-10 20:31:54 -0400

    Added Tile Manager that spawns tiles

---
+++ ShopGame/ShopGame/Assets/Scripts/Tiles/GridManager.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    [SerializeField] private int width, height;
+
+    [SerializeField] private Tile tilePrefab;
+
+    [SerializeField] private Transform cam;
+
+    private Dictionary<Vector2, Tile> tiles;
+
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    void GenerateGrid()
+    {
+        tiles = new Dictionary<Vector2, Tile>();
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Tile spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternion.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+
+                bool isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+
+                spawnedTile.Init(isOffset);
+                tiles[new Vector2(x, y)] = spawnedTile;
+            }
+        }
+
+        cam.transform.position = new Vector3((float)width / 2 - 0.5f, (float)height / 2 - 0.5f, -10);
+    }
+
+    public Tile GetTileAtPosition(Vector2 pos)
+    {
+        if (tiles.TryGetValue(pos, out var tile)) return tile;
+        return null;
+    }
+}
