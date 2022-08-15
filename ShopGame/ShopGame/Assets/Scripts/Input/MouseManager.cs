8e66e01c2ce8aace3dc2fa19d7409047b86f7624
Pyrotaly <102985554+Pyrotaly@users.noreply.github.com>
2022-08-15 12:27:17 -0400

    Mouse Position Manager

--- ShopGame/ShopGame/Assets/Scripts/Input/MouseManager.cs
+++ ShopGame/ShopGame/Assets/Scripts/Input/MouseManager.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MouseManager : MonoBehaviour
 {
+    public Camera mainCam;
+    private Vector3 worldPos;
+
+    Vector3 testPos;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,10 @@ public class MouseManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        worldPos = mainCam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
+        testPos = new Vector2(Mathf.Round(worldPos.x), Mathf.Round(worldPos.y));
+
+        Debug.Log(testPos);
     }
 }
